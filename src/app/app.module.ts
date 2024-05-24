30820432e367e27cac0fef9248f28cc62b7fc79a
Thiago Ferreira <thiago.ferreirarod22@gmail.com>
2024-05-24 11:46:48 -0300

    feat: create project scope

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,18 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { provideAnimationsAsync } from "@angular/platform-browser/animations/async";
+import { AboutComponent } from "./about/about.component";
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { NavbarComponent } from "./components/navbar/navbar.component";
+import { HomeComponent } from "./home/home.component";
+import { MaterialModule } from "./material.module";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent, NavbarComponent, HomeComponent, AboutComponent],
+  imports: [MaterialModule, BrowserModule, AppRoutingModule],
+  providers: [provideAnimationsAsync()],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
